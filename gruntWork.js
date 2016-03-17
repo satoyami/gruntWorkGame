aaa76d35df234f33c3f3b3ece2db4400e34b073f
satoyami <satoyami@yahoo.com>
2016-03-17 12:02:40 -0700

    add loading to weights

--- gruntWork.js
+++ gruntWork.js
@@ -6,15 +6,15 @@ var timecap = 3;
 var distance = 60;
 
 var weights = [
-  {'name':'pink-kb','qty':13,'lbs':15,'pts':1},
-  {'name':'blue-kb','qty':12,'lbs':25,'pts':2},
-  {'name':'yellow-kb','qty':6,'lbs':35,'pts':3},
-  {'name':'plate10','qty':17,'lbs':10,'pts':1},
-  {'name':'plate15','qty':19,'lbs':15,'pts':2},
-  {'name':'plate25','qty':16,'lbs':25,'pts':3},
-  {'name':'plate35','qty':8,'lbs':35,'pts':4},
-  {'name':'plate45','qty':6,'lbs':45,'pts':5},
-  {'name':'bag1','qty':1,'lbs':90,'pts':8}
+  {'name':'pink-kb','qty':13,'lbs':15,'pts':1,'load':2},
+  {'name':'blue-kb','qty':12,'lbs':25,'pts':2,'load':2},
+  {'name':'yellow-kb','qty':6,'lbs':35,'pts':3,'load':2},
+  {'name':'plate10','qty':17,'lbs':10,'pts':1,'load':5},
+  {'name':'plate15','qty':19,'lbs':15,'pts':2,'load':3},
+  {'name':'plate25','qty':16,'lbs':25,'pts':3,'load':3},
+  {'name':'plate35','qty':8,'lbs':35,'pts':4,'load':2},
+  {'name':'plate45','qty':6,'lbs':45,'pts':5,'load':2},
+  {'name':'bag1','qty':1,'lbs':90,'pts':8,'load':1}
 ];
 
 function getTotalWeight(arr){
@@ -36,6 +36,6 @@ weights.forEach(function(w){
   console.log(w);
 });
 
-console.log("Total weight moved per round " + getTotalWeight(weights) + 'lbs');
-console.log("Power output per round " + gruntWork(people,weights,distance,timecap) + 'watts');
+console.log("Total weight moved per round " + getTotalWeight(weights) + ' lbs');
+console.log("Power output per round " + gruntWork(people,weights,distance,timecap) + ' watts');
 //console.log("Total watts per round " + getTotalWeight(weights));
